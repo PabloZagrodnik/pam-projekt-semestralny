95724517d41f7f9c46889516b7208e337f1edbd9
PabloZagrodnik <pawel.zagrodnik0@gmail.com>
2026-01-15 17:38:31 +0100

    feat: implementacja bazy danych Room (Model, DAO, Database)

---
+++ app/src/main/java/com/example/myapplication/data/PlaceDao.kt
@@ -0,0 +1,30 @@
+package com.example.myapplication.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.myapplication.model.Place
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface PlaceDao {
+
+    // Flow - automatyczne odświeżanie listy po zmianach
+    // Zapytanie - zwraca wszystko z tabeli "places"
+    @Query("SELECT * FROM PLACES")
+    fun getAllPlaces(): Flow<List<Place>>
+
+    // Zapytanie - zwraca miejsce (place) o podanym id
+    @Query("SELECT * FROM places WHERE id = :id")
+    suspend fun getPlaceById(id: Int): Place?
+
+    // Wstawianie do tabeli "places"
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertPlace(place: Place)
+
+    // Usuwanie z tabeli "places"
+    @Delete
+    suspend fun deletePlace(place: Place)
+}
---
+++ app/src/main/java/com/example/myapplication/model/Place.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "places")
+data class Place(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val title: String,
+    val description: String,
+    val imageUrl: String,
+    val lat: Double,
+    val lng: Double
+)
